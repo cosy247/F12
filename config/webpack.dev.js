981981d5bc1092cc4ef33d322a0828393875d189
cosy247 <705194673@qq.com>
2023-01-12 17:10:00 +0800

    🔧 build: 取消不必要的loader

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -23,41 +23,6 @@ module.exports = {
                 use: ['html-loader'],
             },
             {
-                test: /\.css$/,
-                use: ['raw-loader'],
-            },
-            {
-                test: /\.less$/,
-                use: ['raw-loader', 'less-loader'],
-            },
-            {
-                test: /\.s[ac]ss$/,
-                use: ['raw-loader', 'less-loader'],
-            },
-            {
-                test: /\.styl$/,
-                use: ['raw-loader', 'stylus-loader'],
-            },
-            {
-                test: /\.(jpe?g|png|gif|webp|svg)$/,
-                type: 'asset',
-                parser: {
-                    dataUrlCondition: {
-                        maxSize: 10 * 1024,
-                    },
-                },
-                generator: {
-                    filename: 'media/[hash:10][ext][query]',
-                },
-            },
-            {
-                test: /\.(ttf|woff2|mp3|mp4|avi)$/,
-                type: 'asset/resourse',
-                generator: {
-                    filename: 'media/[hash:10][ext][query]',
-                },
-            },
-            {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loader: 'babel-loader',
